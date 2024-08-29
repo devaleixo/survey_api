7aba0de8d705d335fb3a4c973105c1b6ef89d32b
Gabriel Aleixo <gabriel.aleixo061@gmail.com>
2024-08-29 11:26:20 -0300

    added seed for predetermined types of questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Type.create!(name: "Radio Button")
+Type.create!(name: "Checkbox")
+Type.create!(name: "Textfield")
